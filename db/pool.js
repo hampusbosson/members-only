d726b97effe9314bd5de9aa74ec4e077dee5beeb
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-19 19:41:22 +0200

    Setup database pool

---
+++ db/pool.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+const query = (text, params) => pool.query(text, params);
+
+module.exports = query;
