7f402a6504ac80cf4ebf88a15b5d034304073131
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-20 22:07:36 +0200

    Make logged-in user available globally in all templates

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(
 );
 app.use(passport.session());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
