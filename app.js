02185ab99461b76cec8396ee8cf4d38fb9d4cc58
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-20 22:40:35 +0200

    Setup connect-pg-simple session store

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const router = require("./routes/routes");
 const session = require("express-session");
 const passport = require("./config/passportConfig");
+const db = require('./db/pool');
+const pgSession = require('connect-pg-simple')(session);
 require("dotenv").config();
 
 const app = express();
@@ -13,11 +15,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
+    store: new pgSession({
+        pool: db.pool
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: { 
+        maxAge: 1000 * 60 * 60 * 24, // 1 day expiration
+        secure: process.env.NODE_ENV === 'production',  // Only secure in production
+        httpOnly: true,  // Prevent access via JavaScript
+        sameSite: 'lax',  // Protect against CSRF
+    }
   }),
 );
+
 app.use(passport.session());
 
 app.use((req, res, next) => {
