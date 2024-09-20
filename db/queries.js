3e5a0a7b33f3801ec9447521f7aef44e1cfbb096
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-20 21:30:50 +0200

    Add query for inserting a user into the db

--- db/queries.js
+++ db/queries.js
@@ -1,4 +1,39 @@
-const query  = require("./pool");
+const query = require("./pool");
+
+async function insertUser(
+    firstName,
+    lastName,
+    username,
+    password
+) {
+    const userInsertQuery = `
+        INSERT INTO users (firstname, lastname, username, password)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id;
+    `;
+
+    const userValues = [
+        firstName,
+        lastName,
+        username,
+        password
+    ];
+
+    try {
+        const result = await query(userInsertQuery, userValues);
+        const userId = result.rows[0].id;
+        console.log('User inserted successfully with ID:', userId);
+
+        return userId;
+    } catch(err) {
+        console.error('Error inserting user:', err.message);
+        throw new Error(`Error inserting user: ${err.message}`);
+    }
+}
+
+module.exports = {
+    insertUser,
+}
 
 
 
