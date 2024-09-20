6dc7d12b979b15457d1cc5f682916c15d66a9122
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-20 21:08:57 +0200

    Set up passport session in app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const router = require('./routes/routes');
+const session = require('express-session');
+const passport = require('./config/passportConfig');
+require('dotenv').config();
 
 const app = express(); 
 
@@ -8,6 +11,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
