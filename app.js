9dfaddff6d7ad85ffbc8c566158bc46ed3c4265f
Hampus Bosson <hampus.bosson@outlook.com>
2024-09-20 21:59:22 +0200

    Refactor middleware setup so that the session middleware is loaded before passport init and routes

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const path = require('path');
-const router = require('./routes/routes');
-const session = require('express-session');
-const passport = require('./config/passportConfig');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const router = require("./routes/routes");
+const session = require("express-session");
+const passport = require("./config/passportConfig");
+require("dotenv").config();
 
-const app = express(); 
+const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use('/', router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 app.use(passport.session());
+
+app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
